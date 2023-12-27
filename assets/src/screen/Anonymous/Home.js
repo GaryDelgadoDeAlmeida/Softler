2b7d8519c11f221fd87cf51872ae21eabd88a5db
Garry ALMEIDA <gary.almeida.work@gmail.com>
2023-12-27 13:02:47 +0100

    Fontend : Work process section + Pricing section

--- assets/src/screen/Anonymous/Home.js
+++ assets/src/screen/Anonymous/Home.js
@@ -1,15 +1,18 @@
 import React from "react";
 import Header from "../../component/part/Header";
+import Carrousel from "../../component/part/Carrousel";
 import ContactForm from "../../component/form/ContactForm";
 import SubscribeForm from "../../component/form/SubscribeForm";
 import CollapseList from "../../component/part/CollapseList";
+import { Link } from "react-router-dom";
 
 export default function Home() {
 
     return (
         <Header>
-            <div className={"hero"}>
-                <div className={"hero-wrapper"}>
+            {/* Header home */}
+            <div className={"page-home-hero"}>
+                <div className={"hero-wrapper page-wrapper"}>
                     <div className={"-left"}>
                         <h1 className={"-title"}>World Famous App<br />Service Provider.</h1>
                         <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
@@ -42,32 +45,196 @@ export default function Home() {
             {/* Trust banner */}
             <div className={"page-section"}>
                 <div className={"page-wrapper"}>
-                    <div className={"trust-banner"}></div>
+                    <div className={"trust-banner"}>
+                        <div className={"trust-banner-wrapper"}>
+                            <div className={"-text"}>
+                                <h3>We're Trusted By</h3>
+                                <p>Join the <span className={"-number"}>200500+</span> Apps Trusting Landy</p>
+                            </div>
+                            <div className={"-bands"}>
+                                <img src={`${window.location.origin}/content/img/client-1.png`} alt={""} />
+                                <img src={`${window.location.origin}/content/img/client-2.png`} alt={""} />
+                                <img src={`${window.location.origin}/content/img/client-3.png`} alt={""} />
+                                <img src={`${window.location.origin}/content/img/client-4.png`} alt={""} />
+                                <img src={`${window.location.origin}/content/img/client-5.png`} alt={""} />
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
 
             {/* Feature */}
-            <div className={"page-section"}>
-                <div className={"page-wrapper"}>
-                    <div className={"d-flex"}>
-                        <div className={"item"}></div>
-                        <div className={"item"}></div>
+            <div className={"page-section page-feature"}>
+                <div className={"feature"}>
+                    <div className={"page-wrapper feature-wrapper"}>
+                        <img className={"feature-left"} src={`${window.location.origin}/content/img/feature-left.png`} alt={""} />
+                        
+                        <div className={"feature-right"}>
+                            <span className={"feature-suptitle"}>Our Features</span>
+                            <h3 className={"feature-title"}>Landu Providing You The Best Features</h3>
+
+                            <div className={"d-grid -col-2 -t-col-1"}>
+                                <div className={"feature-card"}>
+                                    <div className={"-header"}>
+                                        <div className={"-bubble"}>
+                                            <img src={`${window.location.origin}/content/svg/cloud-upload-white.svg`} alt={""} />
+                                        </div>
+                                    </div>
+                                    <div className={"-content"}>
+                                        <h3>Data In Cloud</h3>
+                                        <p>Lorem ipsum dolor sit amet, consectetur</p>
+                                    </div>
+                                </div>
+                                <div className={"feature-card"}>
+                                    <div className={"-header"}>
+                                        <div className={"-bubble"}>
+                                            <img src={`${window.location.origin}/content/svg/id-card-white.svg`} alt={""} />
+                                        </div>
+                                    </div>
+                                    <div className={"-content"}>
+                                        <h3>Exclusive Design</h3>
+                                        <p>Lorem ipsum dolor sit amet, consectetur</p>
+                                    </div>
+                                </div>
+                                <div className={"feature-card"}>
+                                    <div className={"-header"}>
+                                        <div className={"-bubble"}>
+                                            <img src={`${window.location.origin}/content/svg/support-white.svg`} alt={""} />
+                                        </div>
+                                    </div>
+                                    <div className={"-content"}>
+                                        <h3>Live Chat</h3>
+                                        <p>Lorem ipsum dolor sit amet, consectetur</p>
+                                    </div>
+                                </div>
+                                <div className={"feature-card"}>
+                                    <div className={"-header"}>
+                                        <div className={"-bubble"}>
+                                            <img src={`${window.location.origin}/content/svg/comments-white.svg`} alt={""} />
+                                        </div>
+                                    </div>
+                                    <div className={"-content"}>
+                                        <h3>24/7 Support</h3>
+                                        <p>Lorem ipsum dolor sit amet, consectetur</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
 
             {/* App work / app demo */}
+            <div className={"app-banner"}>
+                <div className={"page-wrapper"}>
+                    <div className={"app-banner-wrapper"}>
+                        <span>80 +</span>
+                        <span>500 +</span>
+                        <span>1098 +</span>
+                        <span>25 +</span>
+                    </div>
+                </div>
+            </div>
             <div className={"page-section"}>
-                <div className={"page-wrapper"}></div>
+                <div className={"page-wrapper"}>
+                    <div className={"app"}>
+                        <div className={"app-img"}>
+                            <img className={"app-logo"} src={`${window.location.origin}/content/img/wp-1.png`} alt={""} />
+                            <img className={"rotation"} src={`${window.location.origin}/content/img/w-p.png`} alt={""} />
+                        </div>
+                        <div className={"app-intro"}>
+                            <span className={"app-suptitle"}>Working progress</span>
+                            <h3 className={"app-title"}>How Does This App Work?</h3>
+
+                            <div className={"d-col -g-15"}>
+                                <div className={"work-card"}>
+                                    <div className={"-header"}>
+                                        <div className={"-image"}>
+                                            <img src={`${window.location.origin}/content/svg/file-lines.svg`} alt={""} />
+                                        </div>
+                                    </div>
+                                    <div className={"-content"}>
+                                        <h3>1. Download Free Trial!</h3>
+                                        <p>Lorem Ipsum is simply dummy text of the printing. Lorem Ipsum is simply dummy text of the printing</p>
+                                    </div>
+                                </div>
+                                <div className={"work-card"}>
+                                    <div className={"-header"}>
+                                        <div className={"-image"}>
+                                            <img src={`${window.location.origin}/content/svg/user-white.svg`} alt={""} />
+                                        </div>
+                                    </div>
+                                    <div className={"-content"}>
+                                        <h3>2. Make A Profile</h3>
+                                        <p>Lorem Ipsum is simply dummy text of the printing. Lorem Ipsum is simply dummy text of the printing</p>
+                                    </div>
+                                </div>
+                                <div className={"work-card"}>
+                                    <div className={"-header"}>
+                                        <div className={"-image"}>
+                                            <img src={`${window.location.origin}/content/svg/mobile_graphs-white.svg`} alt={""} />
+                                        </div>
+                                    </div>
+                                    <div className={"-content"}>
+                                        <h3>3. Enjoy The App</h3>
+                                        <p>Lorem Ipsum is simply dummy text of the printing. Lorem Ipsum is simply dummy text of the printing</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
 
-            {/* Witness */}
+            {/* Client Witness */}
             <div className={"page-section bg-lightBlue"}>
                 <div className={"page-wrapper"}>
                     <h2 className={"page-title"}>What our client says ?</h2>
                     <small className={"page-subtitle"}>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</small>
 
-                    <div className={""}></div>
+                    <div className={"carrousel"}>
+                        <div className={"carrousel-wrapper"}>
+                            <div className={"-items"}>
+                                <div className={"client-card"}>
+                                    <div className={"-header"}>
+                                        <img src={`${window.location.origin}/content/img/user-1.png`} alt={""} />
+                                    </div>
+                                    <div className={"-content"}>
+                                        <h4>Jason Momoya</h4>
+                                        <span>Developer</span>
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error suscipit, quos laboriosam nostrum. Autem, natus.</p>
+                                    </div>
+                                </div>
+                                
+                                <div className={"client-card"}>
+                                    <div className={"-header"}>
+                                        <img src={`${window.location.origin}/content/img/user-2.png`} alt={""} />
+                                    </div>
+                                    <div className={"-content"}>
+                                        <h4>Shaila Khanom</h4>
+                                        <span>Designer</span>
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error suscipit, quos laboriosam nostrum. Autem, natus.</p>
+                                    </div>
+                                </div>
+                                
+                                <div className={"client-card"}>
+                                    <div className={"-header"}>
+                                        <img src={`${window.location.origin}/content/img/user-3.png`} alt={""} />
+                                    </div>
+                                    <div className={"-content"}>
+                                        <h4>Philt Jordan</h4>
+                                        <span>Designer</span>
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error suscipit, quos laboriosam nostrum. Autem, natus.</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className={"-pagination"}>
+                                <button className={"-button -active"}></button>
+                                <button className={"-button"}></button>
+                                <button className={"-button"}></button>
+                            </div>
+                        </div>
+                    </div>
 
                     <div className={"subscribe"}>
                         <div className={"-left"}>
@@ -87,7 +254,7 @@ export default function Home() {
                     <h2 className={"page-title"}>Software overview</h2>
                     <small className={"page-subtitle"}>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</small>
 
-                    <div className={"carrousel"}></div>
+                    <Carrousel />
                 </div>
             </div>
 
@@ -112,12 +279,96 @@ export default function Home() {
             <div className={"page-section"}>
                 <div className={"page-wrapper"}>
                     <h2 className={"page-title"}>Pricing table</h2>
-                    <small className={"page-subtitle"}></small>
+                    <small className={"page-subtitle"}>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</small>
 
-                    <div className={"d-col -col-3"}>
-                        <div className={"price"}></div>
-                        <div className={"price"}></div>
-                        <div className={"price"}></div>
+                    <div className={"d-grid -col-3 -t-col-2 -m-col-1"}>
+                        <div className={"item"}>
+                            <div className={"price"}>
+                                <div className={"-sup-header"}>
+                                    <span>Basic</span>
+                                </div>
+                                <div className={"-header"}>
+                                    <div className={"-icon"}>
+                                        <img src={`${window.location.origin}/content/svg/server-white.svg`} alt={""} />
+                                    </div>
+                                </div>
+                                <div className={"-content"}>
+                                    <div className={"-amount"}>
+                                        <span>$130</span>
+                                        <span>per month</span>
+                                    </div>
+                                    <div className={"-description"}>
+                                        <li>Business Analyzing</li>
+                                        <li>24/7 Tech Suport</li>
+                                        <li>Operational Excellence</li>
+                                        <li>Knowledge Management</li>
+                                        <li>Business Idea Ready</li>
+                                    </div>
+                                </div>
+                                <div className={"-footer"}>
+                                    <Link className={"btn btn-outline-white fz-16 f-bold"} to={"#"}>Choice Plan</Link>
+                                </div>
+                            </div>
+                        </div>
+                        <div className={"item"}>
+                            <div className={"price -even"}>
+                                <div className={"-sup-header"}>
+                                    <span>Standard</span>
+                                </div>
+                                <div className={"-header"}>
+                                    <div className={"-icon -even"}>
+                                        <img src={`${window.location.origin}/content/svg/gearwheel-white.svg`} alt={""} />
+                                    </div>
+                                </div>
+                                <div className={"-content"}>
+                                    <div className={"-amount"}>
+                                        <span>$150</span>
+                                        <span>per month</span>
+                                    </div>
+                                    <div className={"-description"}>
+                                        <li>Business Analyzing</li>
+                                        <li>24/7 Tech Suport</li>
+                                        <li>Next Generation Accounting</li>
+                                        <li>Online Staff Training Sessions</li>
+                                        <li>Business Idea Ready</li>
+                                        <li>20 Database</li>
+                                    </div>
+                                </div>
+                                <div className={"-footer"}>
+                                    <Link className={"btn btn-outline-white fz-16 f-bold"} to={"#"}>Choice Plan</Link>
+                                </div>
+                            </div>
+                        </div>
+                        <div className={"item"}>
+                            <div className={"price"}>
+                                <div className={"-sup-header"}>
+                                    <span>Premium</span>
+                                </div>
+                                <div className={"-header"}>
+                                    <div className={"-icon"}>
+                                        <img src={`${window.location.origin}/content/svg/settings-white.svg`} alt={""} />
+                                    </div>
+                                </div>
+                                <div className={"-content"}>
+                                    <div className={"-amount"}>
+                                        <span>$180</span>
+                                        <span>per month</span>
+                                    </div>
+                                    <div className={"-description"}>
+                                        <li>Digital Transformation</li>
+                                        <li>24/7 Tech Suport</li>
+                                        <li>Supply Chain Optimization</li>
+                                        <li>E-mail support</li>
+                                        <li>Business Idea Ready</li>
+                                        <li>Unlimited Database</li>
+                                        <li>Customer Support</li>
+                                    </div>
+                                </div>
+                                <div className={"-footer"}>
+                                    <Link className={"btn btn-outline-white fz-16 f-bold"} to={"#"}>Choice Plan</Link>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
--- assets/src/screen/NotFound.js
+++ assets/src/screen/NotFound.js
@@ -1,8 +1,18 @@
 import React from "react";
+import Header from "../component/part/Header";
+import { Link } from "react-router-dom";
 
 export default function NotFound() {
 
     return (
-        <div>PAGE NOT FOUND</div>
+        <Header>
+            <div className={"page-wrapper"}>
+                <div className={"page-section"}>
+                    <h1 className={"page-title txt-left-important"}>PAGE NOT FOUND</h1>
+                    <p>The page you are looking don't exist</p>
+                    <Link className={"btn btn-violet fz-16"} to={"/"}>Home</Link>
+                </div>
+            </div>
+        </Header>
     )
 }
